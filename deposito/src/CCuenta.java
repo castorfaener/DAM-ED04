361de3aa908911401948f167065e997f2cced660
castorfaener <castorfaener@gmail.com>
2021-02-12 20:30:39 +0100

    Primer commit

---
+++ deposito/src/CCuenta.java
@@ -0,0 +1,122 @@
+
+
+/**
+ * Clase para la gestion de cuentas
+ * @author Franscisco Alacreu
+ */
+public class CCuenta {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+    
+    /**
+     * Constructor de clase
+     */
+    public CCuenta()
+    {
+    }
+    /**
+     * Constructor de clase
+     * @param nom nombre
+     * @param cue cuenta
+     * @param sal saldo
+     * @param tipo tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+    /**
+     * Devuelve el saldo de la cuenta
+     * @return saldo de la cuenta
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+    /**
+     * Ingresa la cantidad pasada por parametro
+     * @param cantidad cantidad a ingresar
+     * @throws Exception En caso de que la cantidad sea negativa
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+    /**
+     * Retira la cantidad pasada por parametro de la cuenta
+     * @param cantidad cantidad a retirar
+     * @throws Exception Si la cantidad es negativa o no hay suficiente saldo
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
---
+++ deposito/src/Main.java
@@ -0,0 +1,37 @@
+
+
+/**
+ * 
+ * @author Francisco Alacreu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+
+        operativa_cuenta();
+    }
+    /**
+     * Controlamos las diferentes acciones a realizar con la cuenta
+     */
+    public static void operativa_cuenta() {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        try {
+            cuenta1.retirar(9600);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
